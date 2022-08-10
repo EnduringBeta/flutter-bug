8981ae5ddc507711d759582d748e2c9534d6af7e
Ross Llewallyn <rossllewallyn@protonmail.com>
2022-08-10 12:12:55 -0400

    integration test for showing bug

---
+++ integration_test/integration_test.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+import 'package:flutter_bug/main.dart' as app;
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('bug demonstration test', () {
+    testWidgets('confirm slider is there, but with hit test + Cupertino, fails',
+            (tester) async {
+          app.main();
+          await tester.pumpAndSettle();
+
+          // Verify the counter starts at 0.
+          expect(find.text('Slider 1'), findsOneWidget);
+          expect(find.byKey(const Key('Slider 1')), findsOneWidget);
+          expect(find.text('Slider 2'), findsOneWidget);
+          expect(find.byKey(const Key('Slider 2')), findsOneWidget);
+          expect(find.text('Slider 3'), findsOneWidget);
+          expect(find.byKey(const Key('Slider 3')), findsOneWidget);
+
+          tester.printToConsole('If iOS/Cupertino sliders, this doesn\'t find '
+              'the sliders...');
+
+          Finder slidersFinder = find.byType(Slider).hitTestable();
+          expect(slidersFinder, findsNWidgets(3));
+        });
+  });
+}
