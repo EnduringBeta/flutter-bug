116adab311e8dd3f788eeac98ab6985e2aa53209
Ross Llewallyn <boss1001@gmail.com>
2022-11-28 12:39:26 -0500

    WIP commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:screenshot/screenshot.dart';
+import 'package:share_plus/share_plus.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,12 +10,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  static const String bugName = "Bug Name";
+  static const String bugName = "Screenshot Text Always Overflows?";
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Flutter Bug/Issue',
+      title: 'Screenshot Text Always Overflows?',
       home: MyHomePage(title: bugName),
     );
   }
@@ -28,9 +31,22 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final ScreenshotController screenshotController = ScreenshotController();
+
   int _counter = 0;
 
-  void _incrementCounter() {
+  void _screenshotShareAndCount() async {
+    // TODOROSS
+    Uint8List bytes = await screenshotController.captureFromWidget(
+      Container(
+          padding: const EdgeInsets.all(30.0),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.blueAccent, width: 5.0),
+            color: Colors.redAccent,
+          ),
+          child: const Text("This is an invisible widget")),
+    );
+    Share.shareXFiles([]);
     setState(() {
       _counter++;
     });
@@ -55,9 +71,9 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: _screenshotShareAndCount,
+        tooltip: 'Screenshot',
+        child: const Icon(Icons.camera_alt),
       ),
     );
   }
