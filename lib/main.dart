c62b9410fe075db68f5e350f9e8675594a2fe5f8
Ross Llewallyn <rossllewallyn@proton.me>
2024-07-25 10:15:02 -0400

    flutter intro bug attempt, but it worked fine dangit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_intro/flutter_intro.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,9 +24,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Bug/Issue',
-      home: MyHomePage(title: bugName),
+    return Intro(
+      child: const MaterialApp(
+        title: 'Flutter Bug/Issue',
+        home: MyHomePage(title: bugName),
+      ),
     );
   }
 }
@@ -34,6 +37,11 @@ class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
+
+  static const String introGroup = 'intro group';
+
+  static const bool shouldDelay = false;
+  static const Duration introGuideDelay = Duration(seconds: 1);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -48,28 +56,64 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _onWidgetLoad(BuildContext context, String group) {
+    final Intro intro = Intro.of(context);
+    if (MyHomePage.shouldDelay) {
+      Future.delayed(MyHomePage.introGuideDelay,
+          () => intro.start(group: group, reset: true));
+    } else {
+      intro.start(group: group, reset: true);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        leading: IntroStepBuilder(
+          order: 3,
+          text: 'Use this to refresh and run the intro again.',
+          group: MyHomePage.introGroup,
+          builder: (_, Key key) => IconButton(
+            key: key,
+            icon: const Icon(Icons.refresh),
+            onPressed: () => Navigator.of(context).pushReplacement(
+                MaterialPageRoute(
+                    builder: (context) =>
+                        const MyHomePage(title: MyApp.bugName))),
+          ),
+        ),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            IntroStepBuilder(
+              order: 2,
+              text: 'The number is here!',
+              group: MyHomePage.introGroup,
+              builder: (_, Key key) => Text(
+                key: key,
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      floatingActionButton: IntroStepBuilder(
+        order: 1,
+        text: 'This is the FAB. Tap it to increase the counter!',
+        group: MyHomePage.introGroup,
+        onWidgetLoad: () => _onWidgetLoad(context, MyHomePage.introGroup),
+        builder: (_, Key key) => FloatingActionButton(
+          key: key,
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        ),
       ),
     );
   }
