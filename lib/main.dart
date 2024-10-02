dd4efe928a0f2e178d88a103df4ba4c66ade38b9
Ross Llewallyn <rossllewallyn@proton.me>
2024-10-01 22:05:53 -0400

    lint

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ Future<void> main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   static const String bugName = "Bug Name";
 
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({super.key, required this.title});
 
   final String title;
 
